6dadbaa87ca0b7ceb35914bdddd2d29d0cbb4974
keijeizei <kctan9@up.edu.ph>
2023-03-06 16:04:53 +0800

    Change secondary color

--- lib/views/utils/AppColor.dart
+++ lib/views/utils/AppColor.dart
@@ -4,7 +4,7 @@ class AppColor {
   static Color primary = const Color(0xFF1C3165);
   static Color primarySoft = const Color(0xFF26438B);
   static Color primaryExtraSoft = const Color(0xFFEEF4F4);
-  static Color secondary = const Color(0xFFEDE5CC);
+  static Color secondary = Color.fromARGB(255, 175, 242, 255);
   static Color whiteSoft = const Color(0xFFF8F8F8);
   static LinearGradient bottomShadow = LinearGradient(colors: [
     const Color(0xFF3A67D7).withOpacity(0.2),
